23a4ae624057e61e8413ff98fa027bf2c196a2c6
quynhnhu478 <lethiquynhnhu537@thptchuyenhatinh.edu.vn>
2026-04-23 20:49:16 +0700

    Delete Source/src/main/java/auction/exception directory

--- Source/src/main/java/auction/exception/AuthException.java
+++
@@ -1,7 +0,0 @@
-package auction.exception;
-public class AuthException extends Exception {
-    public AuthException(String message) {
-        super(message);
-    }
-}
-//check khớp dữ liệu đã lưu login ko nếu ko ném ra exception
