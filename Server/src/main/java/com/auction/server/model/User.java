2e225caecd8b0e19ade0a2d4faf111f85662927e
2502334_TranBichNgoc <tranbichngoc855@gmail.com>
2026-05-04 03:03:13 +0700

    chinh sua lai entity cua user

--- Server/src/main/java/com/auction/server/model/User.java
+++ Server/src/main/java/com/auction/server/model/User.java
@@ -9,9 +9,18 @@ import java.util.Set;
 @Table(name =" user")
 public class User extends BaseEntity {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column
     private String name;
+    @Column
     private String email;
+    @Column
     private String password;
+
     @ManyToMany
     @JoinTable(
             name = "user_roles",
@@ -20,6 +29,9 @@ public class User extends BaseEntity {
     )
     private Set<Roles> roles = new HashSet<>();
     public User(){}
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Item>  items = new HashSet<>();
 
     public User(String name, String email, String password){
         this.name = name;
