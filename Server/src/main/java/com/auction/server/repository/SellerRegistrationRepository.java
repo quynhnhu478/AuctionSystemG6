77defb34216522b138329eb300dafd059dc0c391
NgTranYenNhi <25023350@vnu.edu.vn>
2026-05-20 15:56:00 +0700

    feat: add function on SellerRegistrationRepository

--- Server/src/main/java/com/auction/server/repository/SellerRegistrationRepository.java
+++ Server/src/main/java/com/auction/server/repository/SellerRegistrationRepository.java
@@ -3,6 +3,10 @@ package com.auction.server.repository;
 import com.auction.server.model.User.SellerRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SellerRegistrationRepository extends JpaRepository<SellerRegistration, Long> {
     boolean existsByUser1_IdAndStatus(Long userId, String status);
+    List<SellerRegistration> findByStatusOrderByCreatedAtDesc(String status);
+    List<SellerRegistration> findAllByOrderByCreatedAtDesc();
 }
