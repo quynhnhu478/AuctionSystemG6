8f0eabc95afabdf12bbda12e6e2d44871a18a7ac
2502334_TranBichNgoc <tranbichngoc855@gmail.com>
2026-05-21 15:00:08 +0700

    Them thuoc tinh va gan controller

---
+++ Common/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Common {
+    // Cho phép các module khác (như Client, Server) nhìn thấy các class trong package này
+    exports com.auction.common.enums;
+    exports com.auction.common.payload;
+
+    // Yêu cầu thư viện Jackson để xử lý JSON (nếu trong các class này có dùng)
+    requires tools.jackson.databind;
+
+}
